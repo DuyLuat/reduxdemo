5cff300acb1d69001c18bb7cd3b073a8d18b5ab4
DuyLuat <daoduyluat@gmail.com>
2022-03-16 16:32:54 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import myReducer from './reducers';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 
-ReactDOM.render(
+const store = createStore(myReducer);
+
+ReactDOM.render( <Provider store={store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
+
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
